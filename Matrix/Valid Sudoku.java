36d8123ea307b03f64a89f3fd972c01bed225d6c
Mayank Raj <88675779+mayankraj25@users.noreply.github.com>
2025-09-11 02:38:27 +0530

    Create Valid Sudoku.java

---
+++ Matrix/Valid Sudoku.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows=new boolean[9][9];
+        boolean[][] columns=new boolean[9][9];
+        boolean[][] box=new boolean[9][9];
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.'){
+                    int num=board[i][j]-'1';
+                    int boxIndex=(i/3)*3+(j/3);
+                    if(rows[i][num] || columns[j][num] || box[boxIndex][num]){
+                        return false;
+                    }
+                    rows[i][num]=columns[j][num]=box[boxIndex][num]=true;
+                }
+            }
+        }
+        return true;
+    }
+}
